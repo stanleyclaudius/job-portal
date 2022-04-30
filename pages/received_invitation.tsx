ebf146604b20bb6cfcb1bffe87723e0fb7544fd4
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-04-30 22:14:27 +0700

    Create received invitation page

---
+++ pages/received_invitation.tsx
@@ -0,0 +1,35 @@
+import Head from "next/head"
+import Footer from "../components/general/Footer"
+import Navbar from "../components/general/Navbar"
+import OrganizationCard from "../components/general/OrganizationCard"
+
+const ReceivedInvitation = () => {
+  return (
+    <>
+      <Head>
+        <title>Job Seek | Received Invitation</title>
+      </Head>
+      <Navbar />
+      <div className='md:py-10 py-6 md:px-16 px-8'>
+        <h1 className='text-xl font-medium'>Received Invitation</h1>
+        <div className='mt-6 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 md:gap-10 gap-8'>
+          <OrganizationCard />
+          <OrganizationCard />
+          <OrganizationCard />
+          <OrganizationCard />
+          <OrganizationCard />
+          <OrganizationCard />
+          <OrganizationCard />
+          <OrganizationCard />
+          <OrganizationCard />
+          <OrganizationCard />
+          <OrganizationCard />
+          <OrganizationCard />
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default ReceivedInvitation
