7932b8702a067c51d7005c6ae1c47b8ad1c4347e
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-05-01 13:31:38 +0700

    Create CreateJobModal component

---
+++ components/modal/CreateJobModal.tsx
@@ -0,0 +1,101 @@
+import { useState, useEffect, useRef, FormEvent, ChangeEvent } from "react"
+import { AiOutlineClose } from "react-icons/ai"
+import Editor from "../../utils/Editor"
+
+interface IProps {
+  openModal: boolean
+  setOpenModal: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const CreateJobModal = ({ openModal, setOpenModal }: IProps) => {
+  const [skills, setSkills] = useState<string[]>([])
+  const [description, setDescription] = useState('')
+  const [requirement, setRequirement] = useState('')
+
+  const modalRef = useRef() as React.MutableRefObject<HTMLDivElement>
+
+  const handleChangeSkills = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    // @ts-ignore
+    if (e.key === ',' && e.target.value !== ',') {
+      // @ts-ignore
+      setSkills([...skills, e.target.value.substring(0, e.target.value.length - 1)])
+      // @ts-ignore
+      e.target.value = ''
+    }
+  }
+
+  const handleRemoveSkill = (idx: number) => {
+    const skillCopy = [...skills]
+    skillCopy.splice(idx, 1)
+    setSkills(skillCopy)
+  }
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e: MouseEvent) => {
+      if (openModal && modalRef.current && !modalRef.current.contains(e.target as Node)) {
+        setOpenModal(false)
+      }
+    }
+
+    document.addEventListener('mousedown', checkIfClickedOutside)
+    return () => document.removeEventListener('mousedown', checkIfClickedOutside)
+  }, [openModal])
+  
+  return (
+    <div className={`${openModal ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'} transition-opacity fixed top-0 left-0 bottom-0 right-0 bg-[rgba(0,0,0,.7)] flex items-center justify-center p-10 z-[9999]`}>
+      <div ref={modalRef} className={`${openModal ? 'translate-y-0' : '-translate-y-12'} transition-transform bg-white w-full max-w-[609px] rounded-md h-[600px] overflow-auto hide-scrollbar`}>
+        <div className='flex items-center justify-between px-7 py-5 border-b boder-gray-300'>
+          <h1 className='font-medium text-lg'>Create Job</h1>
+          <AiOutlineClose onClick={() => setOpenModal(false)} className='cursor-pointer' />
+        </div>
+        <div className='p-7'>
+          <form>  
+            <div className='mb-6'>
+              <label htmlFor='position' className='text-sm'>Position</label>
+              <input type='text' id='position' name='position' className='outline-0 border border-gray-300 mt-3 text-sm rounded-md w-full px-2 h-10  ' />
+            </div>
+            <div className='mb-6'>
+              <label htmlFor='skills' className='text-sm'>Skills Required</label>
+              <div className='border border-gray-300 mt-3 rounded-md flex items-center px-2 min-h-20 flex-wrap'>
+                <div className='flex items-center gap-3 flex-wrap my-2'>
+                  {
+                    skills.map((item, idx) => (
+                      <div key={item} className='rounded-md bg-gray-100 px-3 py-1 flex items-center gap-2 break-words'>
+                        <p className='text-sm'>{item}</p>
+                        <div className='bg-gray-300 text-gray-600 w-fit rounded-full p-1 cursor-pointer'>
+                          <AiOutlineClose onClick={() => handleRemoveSkill(idx)} className='text-xs' />
+                        </div>
+                      </div>
+                    ))
+                  }
+                </div>
+                <input type='text' onKeyUp={e => handleChangeSkills(e)} className='outline-0 text-sm w-full px-2 h-10 flex-1' />
+              </div>
+            </div>
+            <div className='mb-6'>
+              <label htmlFor='salary' className='text-sm'>Salary</label>
+              <input type='number' className='outline-0 border border-gray-300 mt-3 rounded-md w-full text-sm px-2 h-10' min={1} /> 
+            </div>
+            <div className='mb-6'>
+              <label htmlFor='description' className='text-sm'>Job Overview</label>
+              <Editor
+                content={description}
+                setContent={setDescription}
+              />
+            </div>
+            <div className='mb-6'>
+              <label htmlFor='requirement' className='text-sm'>Requirements</label>
+              <Editor
+                content={requirement}
+                setContent={setRequirement}
+              />
+            </div>
+            <button className='bg-[#504ED7] hover:bg-[#2825C2] transition-[background] text-sm text-white w-full rounded-md py-3'>Post Job</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CreateJobModal
--- pages/organization/jobs.tsx
+++ pages/organization/jobs.tsx
@@ -5,11 +5,13 @@ import Navbar from "../../components/general/Navbar"
 import JobDetailModal from './../../components/modal/JobDetailModal'
 import DeleteModal from '../../components/modal/DeleteModal'
 import ApplicantModal from '../../components/modal/ApplicantModal'
+import CreateJobModal from '../../components/modal/CreateJobModal'
 
 const OrganizationJobs = () => {
   const [openJobDetailModal, setOpenJobDetailModal] = useState(false)
   const [openDeleteModal, setOpenDeleteModal] = useState(false)
   const [openApplicantModal, setOpenApplicantModal] = useState(false)
+  const [openCreateJobModal, setOpenCreateJobModal] = useState(false)
 
   return (
     <>
@@ -20,7 +22,7 @@ const OrganizationJobs = () => {
       <div className='md:py-10 py-7 md:px-16 px-8'>
         <div className='flex items-center justify-between'>
           <h1 className='text-2xl font-medium'>Job Management</h1>
-          <button className='bg-blue-500 hover:bg-blue-600 transition-[background] text-white text-sm rounded-md px-4 py-2'>Create Job</button>
+          <button onClick={() => setOpenCreateJobModal(true)} className='bg-blue-500 hover:bg-blue-600 transition-[background] text-white text-sm rounded-md px-4 py-2'>Create Job</button>
         </div>
         <div className='overflow-x-auto mt-8'>
           <table className='w-full'>
@@ -66,6 +68,11 @@ const OrganizationJobs = () => {
       <ApplicantModal
         openModal={openApplicantModal}
         setOpenModal={setOpenApplicantModal}
+      />
+
+      <CreateJobModal
+        openModal={openCreateJobModal}
+        setOpenModal={setOpenCreateJobModal}
       />
     </>
   )
